9c6389b756a048314ea651d083254274e25a9f7c
jimmy-moringa <jimmy.musyoki@student.moringaschool.com>
2024-10-26 07:51:47 +0300

    done

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from app import db
+from extensions import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///late_show.db'
@@ -12,6 +13,30 @@ migrate = Migrate(app, db)  # Set up Flask-Migrate
 
 from models import Episode, Guest, Appearance
 from routes import *
+@app.route('/episodes', methods=['GET'])
+def get_episodes():
+    episodes = Episode.query.all()
+    return jsonify([episode.to_dict() for episode in episodes])
 
+@app.route('/guests', methods=['GET'])
+def get_guests():
+    guests = Guest.query.all()
+    return jsonify([guest.to_dict() for guest in guests])
+
+@app.route('/appearances', methods=['POST'])
+def create_appearance():
+    data = request.get_json()
+    rating = data.get('rating')
+    episode_id = data.get('episode_id')
+    guest_id = data.get('guest_id')
+
+    # Validate rating
+    if rating < 1 or rating > 5:
+        return jsonify({"errors": ["Rating must be between 1 and 5"]}), 400
+
+    appearance = Appearance(rating=rating, episode_id=episode_id, guest_id=guest_id)
+    db.session.add(appearance)
+    db.session.commit()
+    return jsonify(appearance.to_dict()), 201
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -45,3 +45,47 @@ class Appearance(db.Model):
             "episode": self.episode.to_dict(),
             "guest": self.guest.to_dict()
         }
+
+# models.py
+from extensions import db  # Make sure to import db
+
+class Episode(db.Model):
+    __tablename__ = 'episodes'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "date": self.date,
+            "number": self.number
+        }
+
+class Guest(db.Model):
+    __tablename__ = 'guests'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    occupation = db.Column(db.String, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "occupation": self.occupation
+        }
+
+class Appearance(db.Model):
+    __tablename__ = 'appearances'
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)  # Make sure to validate rating between 1 and 5
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "rating": self.rating,
+            "episode_id": self.episode_id,
+            "guest_id": self.guest_id
+        }
